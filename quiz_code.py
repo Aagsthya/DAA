6de4ba5bfb31be8169d5bab9407991b7f3c18f52
Aagsthya <101513454+Aagsthya@users.noreply.github.com>
2023-09-04 15:20:18 +0530

    Create quiz_code.py
    This is Quiz code for Case Study

---
+++ quiz_code.py
@@ -0,0 +1,72 @@
+import random
+
+#this function will return the key-value pair where key is the word and defination is the value
+def get_def_and_pop(word_list, word_dict):
+    random_index = random.randrange(len(word_list))
+    word = word_list.pop(random_index)
+    definition = word_dict.get(word)
+    return word,definition #return word and definition as a tuple
+
+#split the string into word and definition
+def get_word_definition(rawstring):
+    word, definition = rawstring.split(',', 1)
+    return word, definition
+
+#read file of words and definition
+fh = open("Vocabulary_list.csv", "r")
+wd_list = fh.readlines()
+
+#create file of words and definition without duplicate entries
+#wd_list.pop(0)
+wd_set = set(wd_list)
+fh = open("Vocabulary_set.csv", "w")
+fh.writelines(wd_set)
+
+#creating dictionary
+word_dict = dict()
+for rawstring in wd_set:
+    word, definition = get_word_definition(rawstring)
+    word_dict[word] = definition
+    
+count = 0
+wrong = 0
+
+#loop for creating quiz question with multiple choices
+while True:
+    wd_list = list(word_dict) #list of words
+    choice_list = []#choicelist of definitions
+    for x in range(4):#max 4 choices
+        word, definition = get_def_and_pop(wd_list, word_dict)
+        choice_list.append(definition)
+    #shuffle the choices
+    random.shuffle(choice_list)
+    #printing a word as a quiz question
+    print(word)
+    print("-------------")
+    #printing the choices using for each loop
+    for idx, choice in enumerate(choice_list):
+        #printing index
+        print(idx+1, choice)
+    #getting input from the user
+    choice = int(input("Enter 1,2,3,4; 0 to exit: "))
+    #if user selects the correct choice
+    if choice_list[choice-1] == definition:
+        print("Correct!\n")
+        count+=1
+    #if user wants to exit
+    elif choice == 0:
+        break
+    #if user selects the wrong choice
+    else:
+        print("Incorrect You lose this\n")
+        print("Correct Answer is: ",definition)
+        wrong+=1
+print("Correct number of answers: ", count)
+print("Wrong number of answers: ", wrong)
+tot = count+wrong
+print("Total Score = ",count,"/",tot)
+per = count/tot
+if per >= 0.5:
+    print("You Passed!")
+else:
+    print("You Failed")
